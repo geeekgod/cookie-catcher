205c3446456869a1ab27a76bdcdb15214f260efc
Rishabh Singh <ris8645208@gmail.com>
2024-08-23 11:18:39 +0530

    feat: add logs in file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
 
@@ -13,9 +14,20 @@ app.get("/receive", (req, res) => {
   console.log("Received API request");
   const queryParams = req.query;
   console.log(queryParams);
+  // Store the Query Params in a file with Timestamp
+  const fileString = `\n=========================
+Timestamp: ${new Date().toISOString()}
+Query Params: ${JSON.stringify(queryParams)}
+=========================\n`;
+
+  fs.appendFile("query-logs.txt", fileString, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
   res.send("Received API request");
 });
 
 app.listen(4444, () => {
-  console.log("Server is running on port 4444");
+  console.log("Server is running on port http://localhost:4444");
 });
